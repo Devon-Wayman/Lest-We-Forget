ae314b6145bfa5aa6064ca19285446cb356bded7
Devon Wayman <devonwayman97@gmail.com>
2021-03-27 23:49:51 -0400

    Engine Update -DW
    Updated engine version to 2020.3 and removed the user settings interaction system.

--- Assets/Scripts/Settings/GameSettings.cs
+++
@@ -1,6 +0,0 @@
-﻿// Author: Devon Wayman - December 2020
-public class GameSettings {
-    public bool teleportMovement; // 0 for continuous, 1 for teleport
-    public bool snapTurnMovement; // 0 for snap, 1 for continuous
-    public bool postProcessEnabled; // 0 for disabled, 1 for enabled
-}
